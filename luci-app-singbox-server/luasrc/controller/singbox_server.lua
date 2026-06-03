de43b0b1ae4d79891f374ca6424cc46959845692
haixing1001@gmail.com <haixing1001@gmail.com>
2026-06-04 05:42:54 +0800

    claude

--- luci-app-singbox-server/luasrc/controller/singbox_server.lua
+++
@@ -1,6 +0,0 @@
-module("luci.controller.singbox_server", package.seeall)
-
-function index()
-    if not nixio.fs.access("/etc/config/singbox_server") then return end
-    entry({"admin", "services", "singbox_server"}, cbi("singbox_server/server"), _("Sing-Box Server"), 99).dependent = true
-end
